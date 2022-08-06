b2e3182b5f7374949260e7848859609998884cf1
shihab <shihabchikkushihab@gmail.com>
2022-08-06 22:16:53 +0530

    navnbar partly completed

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './navbar.scss'
+import React from "react";
+import "./navbar.scss";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="item">
+          Hi, User <AccountCircleIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "../../Component/Navbar/Navbar";
 import Sidebar from "../../Component/Sidebar/Sidebar";
 import "./home.scss";
 
@@ -6,7 +7,7 @@ const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">Container</div>
+      <div className="homeContainer"><Navbar/>homecon</div>
     </div>
   );
 };
