094d03ea18d6e3a899e002889893711156e041d5
shihab <shihabchikkushihab@gmail.com>
2022-08-06 19:18:47 +0530

    Initail Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./Component/Login/Login";
+import Register from "./Component/Register/Register";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Main</h1>
+        <Router><Link to={'/Register'}></Link>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+          </Routes>
+          <Routes>
+            <Route exact path="/" element={<Register />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
