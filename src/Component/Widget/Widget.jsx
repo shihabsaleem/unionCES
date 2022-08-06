986ec5293184a483070f11c47a042ba52937b840
shihab <shihabchikkushihab@gmail.com>
2022-08-06 23:07:42 +0530

    reusable widgets

---
+++ src/Component/Widget/Widget.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./widget.scss";
+
+const Widget = () => {
+  return (
+    <div>
+      <div className="widget">
+        <div className="left">left</div>
+        <div className="right">right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Navbar from "../../Component/Navbar/Navbar";
 import Sidebar from "../../Component/Sidebar/Sidebar";
+import Widget from "../../Component/Widget/Widget";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer"><Navbar/>homecon</div>
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget/>
+          <Widget/>
+          <Widget/>
+          <Widget/>
+        </div>
+      </div>
     </div>
   );
 };
