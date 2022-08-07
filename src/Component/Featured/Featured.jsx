13595f8e672132d0ca131f042193717ef0065739
shihab <shihabchikkushihab@gmail.com>
2022-08-07 12:32:57 +0530

    progress

--- src/Component/Featured/Featured.jsx
+++ src/Component/Featured/Featured.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './featured.scss'
+import React from "react";
+import "./featured.scss";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const Featured = () => {
   return (
-    <div className='featured'>Featured</div>
-  )
-}
+    <div className="featured">
+      <div className="top">
+        <h3 className="title>">Polling</h3>
+      </div>
+      <div className="bottom">
+        <div className="featureChart">
+          <CircularProgressbar value={80} text="80%" strokeWidth={5} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
