1827e55ce122329b2e6f0dd5945cc0f56911e8e4
shihab <shihabchikkushihab@gmail.com>
2022-08-07 15:16:26 +0530

    admin nav completed

--- src/Pages/Add/Add.jsx
+++ src/Pages/Add/Add.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./add.scss";
+import Sidebar from "../../Component/Sidebar/Sidebar";
+import Navbar from "../../Component/Navbar/Navbar";
 
 const Add = () => {
-  return <div>Add</div>;
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
 
 export default Add;
--- src/Pages/Result/Result.jsx
+++ src/Pages/Result/Result.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './result.scss'
+import Navbar from '../../Component/Navbar/Navbar'
+import Sidebar from '../../Component/Sidebar/Sidebar'
 
 const Result = () => {
   return (
-    <div>Result</div>
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+      </div>
+    </div>
   )
 }
 
