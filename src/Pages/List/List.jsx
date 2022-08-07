729f56197bae56355e659313152ecf0dcb321307
shihab <shihabchikkushihab@gmail.com>
2022-08-07 15:11:43 +0530

    List Completed

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './list.scss'
+import Sidebar from '../../Component/Sidebar/Sidebar'
+import Navbar from '../../Component/Navbar/Navbar'
+import Table from '../../Component/Table/Table'
+
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+       
+        
+        <div className="listContainer">
+          <div className="listTitle">Poll Result
+          <Table/></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
