d2ec3ccbc23d18d7075414c29f3346599889194c
shihab <shihabchikkushihab@gmail.com>
2022-09-06 00:27:32 +0530

    multi color removed

--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -37,10 +37,7 @@ const Sidebar = () => {
           </li>
         </ul>
       </div>
-      <div className="bottom">
-        <div className="colorOption"></div>
-        <div className="colorOption"></div>
-      </div>
+      
     </div>
   );
 };
--- src/Pages/Add/Add.jsx
+++ src/Pages/Add/Add.jsx
@@ -2,13 +2,51 @@ import React from "react";
 import "./add.scss";
 import Sidebar from "../../Component/Sidebar/Sidebar";
 import Navbar from "../../Component/Navbar/Navbar";
+import logo from "./logo.svg";
 
 const Add = () => {
   return (
-    <div className="home">
+    <div className="add">
       <Sidebar />
-      <div className="homeContainer">
+      <div className="addContainer">
         <Navbar />
+        <div className="top">
+          <h1>Candidate Nomination</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src={logo} />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label>ID</label>
+                <input type="text" placeholder="Admission No" />
+              </div>
+              <div className="formInput">
+                <label>Nominee</label>
+                <input type="text" placeholder="Full Name" />
+              </div>
+              <div className="formInput">
+                <label>Department</label>
+                <input type="text" placeholder="CS/ME/EE/EC" />
+              </div>
+              <div className="formInput">
+                <label>Semester</label>
+                <input type="number" placeholder="2/4/6/8/" />
+              </div>
+              <div className="formInput">
+                <label>Seat</label>
+                <input type="text" placeholder="Post" />
+              </div>
+              <div className="formInput">
+                <label>Profile</label>
+                <textarea type="text" placeholder="Bio" />
+              </div>
+              <button>send</button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
