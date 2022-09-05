5ff63b0c659d4015691696ad986d7ff3d589a30f
shihab <shihabchikkushihab@gmail.com>
2022-09-05 20:24:46 +0530

    working on list, data table added

---
+++ src/Component/Datatable/datatable.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import "./Datatable";
+
+const datatable = () => {
+  return <div className="datatable">datatable</div>;
+};
+
+export default datatable;
