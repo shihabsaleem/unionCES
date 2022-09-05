fe778bb2008e2174002accde920b59dfaeab815c
shihab <shihabchikkushihab@gmail.com>
2022-09-05 21:46:05 +0530

    list

--- src/Component/Datatable/datatable.jsx
+++ src/Component/Datatable/datatable.jsx
@@ -1,47 +1,55 @@
 import React from "react";
+import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import "./Datatable";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
+  { field: "id", headerName: "Adm No", width: 150 },
+  { field: "name", headerName: "Name", width: 300 },
+  { field: "dept", headerName: "Department", width: 150 },
+  { field: "sem", headerName: "Semester", width: 100 },
+  { field: "seat", headerName: "Seat", width: 150 },
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  {
+    id: 2019001,
+    name: "John Doe",
+    dept: "CS",
+    sem: 7,
+    seat: "Chair",
+    action: "Delete",
+  },
+  {
+    id: 2019002,
+    name: "Jon Doe",
+    dept: "EE",
+    sem: 7,
+    seat: "Chair",
+    action: "Delete",
+  },
 ];
 
 const datatable = () => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="delButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
   return (
     <div className="datatable">
       <DataGrid
         rows={rows}
-        columns={columns}
-        pageSize={5}
+        columns={columns.concat(actionColumn)}
+        pageSize={8}
         rowsPerPageOptions={[5]}
         checkboxSelection
       />
