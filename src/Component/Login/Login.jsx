e08a52ba6ec66b7ac8e77fabaec01b4db04f1f6e
shihabsaleem <shihabchikkushihab@gmail.com>
2023-08-04 02:55:37 +0530

    redesign completed

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -4,15 +4,15 @@ import jwt_decode from "jwt-decode";
 import { Navigate, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div >
+    <div>
       <GoogleLogin
         onSuccess={(response) => {
           console.log(response);
           var decoded = jwt_decode(response.credential);
-          navigate("/voter")
-          console.log(decoded)
+          navigate("/voter");
+          console.log(decoded);
         }}
         onError={() => {
           console.log("Login Failed");
